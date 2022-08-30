482d41764ad20a96e96e3f4809e04bf541fbfa77
Benjamin Peinhardt <benjaminpeinhardt@gmail.com>
2022-08-29 20:46:55 -0500

    woowee

--- usage/src/main.rs
+++ usage/src/main.rs
@@ -5,10 +5,12 @@ use web_sys::HtmlInputElement;
 
 use yew::prelude::*;
 
-#[derive(Debug, Clone, YForm)]
+#[derive(Debug, Clone, PartialEq, YForm)]
 pub struct Data {
+    pub name: Option<String>,
     pub email: String,
     pub agree_to_terms: bool,
+    pub subscribe_to_updates: Option<bool>,
 }
 
 #[function_component(Index)]
@@ -19,7 +21,19 @@ pub fn index() -> Html {
         log!(msg);
     });
 
-    html! { <DataForm {onsubmit} /> }
+    // This part is optional, but sometimes you want to have 
+    // some defaults already set. Chances are you'll pull this
+    // from an api request as well.
+    let init = Data {
+        name: None,
+        email: String::new(),
+        agree_to_terms: false,
+
+        // Have this checked by default
+        subscribe_to_updates: Some(true),
+    };
+
+    html! { <DataForm {onsubmit} {init} /> }
 }
 
 fn main() {
